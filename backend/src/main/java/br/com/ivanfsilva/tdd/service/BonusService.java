b7f52027022e376087dc924b94d6175e38b95d9e
ivanfsilva <ivanfs27@gmail.com>
2021-11-16 10:34:47 -0300

    testing a feature

--- backend/src/main/java/br/com/ivanfsilva/tdd/service/BonusService.java
+++ backend/src/main/java/br/com/ivanfsilva/tdd/service/BonusService.java
@@ -1,6 +1,7 @@
 package br.com.ivanfsilva.tdd.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import br.com.ivanfsilva.tdd.modelo.Funcionario;
 
@@ -11,7 +12,8 @@ public class BonusService {
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
 			valor = BigDecimal.ZERO;
 		}
-		return valor;
+
+		return valor.setScale( 2, RoundingMode.HALF_UP );
 	}
 
 }
